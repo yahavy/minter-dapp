e3b45afefa76275e01e98634a424cf612a4bf9bb
yahavy <62987078+yahavy@users.noreply.github.com>
2022-04-23 19:36:30 +0300

    setup

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x5CAac014CD978125413FD4B80d85EaA96Df48B84"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
